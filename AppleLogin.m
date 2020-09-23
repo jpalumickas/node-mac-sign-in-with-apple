ac1d254fcbd3fc07b87f85271731a12166673d71
saimis <simonas.belickas@gmail.com>
2020-09-23 22:23:35 +0300

    cleanup

--- AppleLogin.m
+++ AppleLogin.m
@@ -5,37 +5,32 @@
 @end
 
 @implementation AppleLogin
- -(id)initWithWindow:(NSWindow *)window
-  {
-      if(self = [super init]) {
-        self.window = window;
-      }
-      return self;
+ -(id)initWithWindow:(NSWindow *)window {
+    if(self = [super init]) {
+      self.window = window;
+    }
+    return self;
   }
 
   - (void)initiateLoginProcess:(void (^)(NSDictionary *result))completionHandler errorHandler:(void (^)(NSError *error))errorHandler {
-      self.successBlock = completionHandler;
-      self.errorBlock = errorHandler;
+    self.successBlock = completionHandler;
+    self.errorBlock = errorHandler;
 
-      ASAuthorizationAppleIDProvider *appleIDProvider = [[ASAuthorizationAppleIDProvider alloc]init];
-      ASAuthorizationAppleIDRequest *request = [appleIDProvider createRequest];
-      request.requestedScopes = @[ASAuthorizationScopeFullName, ASAuthorizationScopeEmail];
+    ASAuthorizationAppleIDProvider *appleIDProvider = [[ASAuthorizationAppleIDProvider alloc]init];
+    ASAuthorizationAppleIDRequest *request = [appleIDProvider createRequest];
+    request.requestedScopes = @[ASAuthorizationScopeFullName, ASAuthorizationScopeEmail];
 
-      ASAuthorizationController *authorizationController = [[ASAuthorizationController alloc]initWithAuthorizationRequests:@[request]];
-      authorizationController.delegate = self;
-      authorizationController.presentationContextProvider = self;
+    ASAuthorizationController *authorizationController = [[ASAuthorizationController alloc]initWithAuthorizationRequests:@[request]];
+    authorizationController.delegate = self;
+    authorizationController.presentationContextProvider = self;
 
-      [authorizationController performRequests];
+    [authorizationController performRequests];
   }
-
-  #pragma Authorization Delegates
 
   - (void)authorizationController:(ASAuthorizationController *)controller didCompleteWithAuthorization:(ASAuthorization *)authorization {
     ASAuthorizationAppleIDCredential *appleIDCredential = [authorization credential];
 
     if(appleIDCredential) {
-      NSLog(@"Successfully retrieved user credentials");
-
       NSString *idToken = [[NSString alloc]initWithData:appleIDCredential.identityToken encoding:NSUTF8StringEncoding];
 
       NSString *email = [appleIDCredential valueForKey:@"email"] ?: @"";
@@ -47,18 +42,19 @@
       NSString *lastName = [fullName valueForKeyPath:@"familyName"] ?: @"";
 
 
-      NSDictionary *userDetails = @{@"userIdentifier": [appleIDCredential user], @"firstName": firstName, @"lastName": lastName, @"email" : email, @"identityToken" : idToken};
+      NSDictionary *userDetails = @{
+        @"userIdentifier": [appleIDCredential user],
+        @"firstName": firstName, @"lastName": lastName,
+        @"email" : email, @"identityToken" : idToken
+      };
 
       self.successBlock(userDetails);
     }
   }
 
   - (void)authorizationController:(ASAuthorizationController *)controller didCompleteWithError:(NSError *)error {
-      NSLog(@"Something went wrong: %@",error);
-      self.errorBlock(error);
+    self.errorBlock(error);
   }
-
-  #pragma PresentationAnchorForAuthorizationController Delegate
 
   -(ASPresentationAnchor)presentationAnchorForAuthorizationController:(ASAuthorizationController *)controller {
     return self.window;
