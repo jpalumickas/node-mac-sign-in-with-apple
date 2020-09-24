33301ed709b5330f319a86033662590d9caf6b09
Justas Palumickas <jpalumickas@gmail.com>
2020-09-24 15:13:16 +0300

    Remove console.log

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const appleLogin = require('bindings')('main.node');
 
 const signInWithApple = async (mainWindow) => {
-  console.log('main window', mainWindow);
   return await appleLogin.signInWithApple(mainWindow);
 };
 
