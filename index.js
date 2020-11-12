ccec35760f9782f65ca7283b068d1cb2a6179d9e
Justas Palumickas <jpalumickas@gmail.com>
2020-11-12 13:36:00 +0200

    Do not load on other platforms

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const appleLogin = require('bindings')('main.node');
+const appleLogin = process.platform === 'darwin' ? require('bindings')('main.node') : undefined;
 
 const signInWithApple = async (mainWindow) => {
+  if (process.platform !== 'darwin') {
+    throw new Error('node-mac-sign-in-with-apple only works on macOS')
+  }
+
   return await appleLogin.signInWithApple(mainWindow);
 };
 
